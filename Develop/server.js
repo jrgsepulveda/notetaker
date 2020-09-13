84c7ea5dd2c7e6e5e4a54524b18604077ad92486
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-09-13 16:38:56 -0400

    Set up Server

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+// need express to interact with the front end
+const express = require("express");
+// need path for filename paths
+const path = require("path");
+// need fs to read and write to files
+const fs = require("fs");
+
+// creating an "express" server
+const app = express();
+// Sets an Initial port for listeners
+const PORT = process.env.PORT || 3000;
+
+//  Initialize notesData
+
+let notesData = [];
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./public")));
+
